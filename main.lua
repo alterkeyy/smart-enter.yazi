412eb6bb5fa1dce146846db6247fd35b6abf869c
alterk <alterkey@qq.com>
2026-02-11 23:27:51 +0800

    fix: default behavior which is set in @keymap.toml to enter any folder

--- main.lua
+++ main.lua
@@ -1,14 +1,26 @@
 --- @since 25.5.31
 --- @sync entry
 
-local function setup(self, opts) self.open_multi = opts.open_multi end
+local function setup(self, opts)
+	self.open_multi = opts.open_multi
+	self.innermost = opts.innermost
+end
 
 local function entry(self)
 	local h = cx.active.current.hovered
-	if h and h.cha.is_dir then
-		local url = h.url
+	if not h or not h.cha.is_dir then
+		return ya.emit("open", { hovered = not self.open_multi })
+	end
+
+	local url = h.url
+	local changed = false
+	if self.innermost then
 		while true do
-			local output, err = Command("ls"):args({ "-1p", tostring(url) }):stdout(Command.PIPED):stderr(Command.PIPED):output()
+			local output, err = Command("ls")
+				:args({ "-1p", tostring(url) })
+				:stdout(Command.PIPED)
+				:stderr(Command.PIPED)
+				:output()
 			if not output or not output.status.success then
 				break
 			end
@@ -23,11 +35,16 @@ local function entry(self)
 			end
 
 			url = url / lines[1]:sub(1, -2)
+			changed = true
 		end
-		return ya.emit("cd", { tostring(url) })
 	end
 
-	ya.emit("open", { hovered = not self.open_multi })
+	if changed then
+		ya.emit("cd", { tostring(url) })
+	else
+		ya.emit("enter", {})
+	end
 end
 
 return { entry = entry, setup = setup }
+ 
